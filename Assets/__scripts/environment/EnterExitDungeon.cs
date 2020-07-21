6543d36534eb78f0703c38d8ea2d898dbae2f89b
muratboziev@yandex.ru <muratboziev@yandex.ru>
2020-07-21 23:23:09 +0300

    dungeon enter script1

--- Assets/__scripts/environment/EnterExitDungeon.cs
+++ Assets/__scripts/environment/EnterExitDungeon.cs
@@ -4,22 +4,78 @@ using UnityEngine;
 
 public class EnterExitDungeon : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    bool mobile_input;
+    int cave_layer_mask;
+    Camera camera;
+    
+    Vector2 click_pos_2d = new Vector2();
+    Vector3 touch_pos = new Vector3();
+    List<Vector3> touch_pos_list = new List<Vector3>();
+
+    bool click_condition = false;
+
+    string gui_text;
+
+    GUIStyle guiStyle = new GUIStyle();
+
     void Start()
     {
-        
+        guiStyle.fontSize = 50;
+
+        camera = FindObjectOfType<Camera>() as Camera;
+        cave_layer_mask = 1 << LayerMask.NameToLayer("Dungeon_InOut");
+        mobile_input = (Application.platform == RuntimePlatform.Android);
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        click_condition = false;
+
+#if UNITY_EDITOR
+#endif
+        if (mobile_input == false && Input.GetMouseButtonDown(0))
+        {
+            click_condition = true;
+            touch_pos_list.Add(Input.mousePosition);
+        }
+        else if (mobile_input == true && Input.touchCount > 0)
+        {
+            click_condition = true;
+            foreach (Touch t in Input.touches)
+            {
+                touch_pos_list.Add(t.position);
+            }
+        }
+
+        if (click_condition)
+        {
+            foreach (Vector3 cur_touch_pos in touch_pos_list)
+            {
+                touch_pos = camera.ScreenToWorldPoint(cur_touch_pos);
+
+                click_pos_2d.x = touch_pos.x;
+                click_pos_2d.y = touch_pos.y;
+
+                RaycastHit2D hit = Physics2D.Raycast(click_pos_2d, Vector2.zero, 100, cave_layer_mask);
+                if (hit.collider != null)
+                {
+                    Debug.Log(hit.collider.gameObject.name);
+                    gui_text = hit.collider.gameObject.name;
+                }
+                else
+                    gui_text = "nope";
+            }
+
+            touch_pos_list.Clear();
+        }
+
     }
 
-    void OnMouseDown()
+    private void OnGUI()
     {
-        // this object was clicked - do something
-        Destroy(this.gameObject);
+        GUI.Label(new Rect(10, 10, 100, 20), gui_text, guiStyle);
     }
 
 
